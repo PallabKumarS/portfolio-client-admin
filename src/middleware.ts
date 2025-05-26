d469919c2838b8a28a2209b44e9818718c9c8736
PallabKumarS <pallabkumar26@gmail.com>
2025-05-27 02:23:01 +0600

    feat: Update admin email in authorized list
    This commit updates the authorized email address for admin access from "admin@gmail.com" to "admin@admin.com" in `src/middleware.ts`. This ensures that only the correct admin email is authorized to access admin-protected routes.

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,7 +6,7 @@ const authRoutes = ["/login"];
 const authorizedEmail = [
   "pallabkumar26@gmail.com",
   "pallabkumar2699@gmail.com",
-  "admin@gmail.com",
+  "admin@admin.com",
 ];
 
 export const middleware = async (request: NextRequest) => {
